427a8ec136e69e0187fdc51a7fee85198434cf94
Kevin Helliwell <kbh789@gmail.com>
2024-11-08 15:03:58 -0500

    preparing the app for state management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Kevin" body="React.js is awesome!" />
-      <Post author="Manny" body="Check out Next.js too!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Kevin" body="React.js is awesome!" />
+        <Post author="Manny" body="Check out Next.js too!" />
+      </ul>
+    </>
   );
 }
 
